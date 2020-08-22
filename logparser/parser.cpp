a9f20616ada8fc3d78b6af2eeda54cfce8e0788e
O-seonsik <dhtjstlr777@gmail.com>
2020-08-22 02:11:32 +0000

    Update parser

--- logparser/parser.cpp
+++ logparser/parser.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
 
@@ -9,6 +10,32 @@ void usage(){
 	printf("exam : logparser /var/log/suricata/fast.log\n");
 }
 
+int line_count = 0, fileLen = -1;
+
+int statusObserver(char* path){
+    string read_line;
+    ifstream fread;
+    fread.open(path);
+    while(!fread.is_open()) {
+        printf("can not open %s\n", path);
+        return 1;
+    }
+
+    int i=0;
+    while(!fread.eof()) {
+        i++;
+        getline(fread, read_line);
+        if(fileLen < i){
+            fileLen++;
+            cout << read_line << endl;
+        }
+    }
+    fileLen -= 1;
+    
+    fread.close();
+    
+    return 0;
+}
 
 int main(int argc, char** argv)
 {
@@ -20,18 +47,23 @@ int main(int argc, char** argv)
 	string read_line;
 	ifstream fread;
 	fread.open(argv[1]);
+    int line_count = 0, fileLen=0;
+
 	if(!fread.is_open()) {
-		printf("xx파일이 열리지 않음\n");
+		printf("can not open %s\n", argv[1]);
 		exit(0);
 	}
 
-	while(!fread.eof()) { //한 줄씩 출력
+	while(!fread.eof()) { 
+        fileLen++;
 		getline(fread, read_line);
-		cout<<read_line<<endl;
 	}
+    fileLen -= 1;
 	fread.close();
-
     
-    
+    while(true){
+        sleep(1);
+        statusObserver(argv[1]);
+    }
 	return 0;
 }
